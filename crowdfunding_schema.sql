7d98dec56aebf326d2694bd3e77ed21c5e2a7bd3
Lindsay McFadyen <lindsay@Lindsays-MacBook-Air.local>
2024-02-05 19:09:02 -0600

    project

---
+++ crowdfunding_schema.sql
@@ -0,0 +1,7 @@
+SELECT * FROM campaign limit 10
+
+SELECT * FROM category limit 10
+
+SELECT * FROM subcategory limit 10
+
+SELECT * FROM contacts limit 10 
--- crowdfunding_table_schema.sql
+++ crowdfunding_table_schema.sql
@@ -1,15 +1,20 @@
 --Crowdfunding Table Schema 
 
+DROP TABLE if exists category ;
 CREATE TABLE category(
 	category_id VARCHAR(40) PRIMARY KEY,
 	category VARCHAR(40) NOT NULL
 ); 
 
+
+DROP TABLE if exists subcateogry;
 CREATE TABLE subcategory(
 	subcategory_id VARCHAR(40) PRIMARY KEY,
 	subcategory VARCHAR(40) NOT NULL
 );
 
+
+DROP TABLE if exists contacts;
 CREATE TABLE contacts(
 	contact_id int PRIMARY KEY,
 	first_name VARCHAR(40) NOT NULL,
@@ -17,13 +22,15 @@ CREATE TABLE contacts(
 	email VARCHAR(60) NOT NULL
 );
 
+
+DROP TABLE if exists campaign;
 CREATE TABLE campaign(
 	cf_id int PRIMARY KEY,
 	contact_id int NOT NULL,
 	company_name VARCHAR(40) NOT NULL,
-	description VARCHAR (40) NOT NULL,
-	goal DECIMAL(10,2) NOT NULL,
-	pledged DECIMAL(10,2) NOT NULL,
+	description VARCHAR (400) NOT NULL,
+	goal NUMERIC(10,2) NOT NULL,
+	pledged NUMERIC(10,2) NOT NULL,
 	outcome VARCHAR (40) NOT NULL,
 	backers_count int NOT NULL,
 	country VARCHAR (40) NOT NULL,
